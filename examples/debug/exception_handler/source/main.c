8e65b98d91d58eaf482cefe842d759b260af4a48
Antonio Niño Díaz <antonio_nd@outlook.com>
2024-07-19 01:44:45 +0100

    WIP

--- examples/debug/exception_handler/source/main.c
+++ examples/debug/exception_handler/source/main.c
@@ -1,6 +1,23 @@
 // SPDX-License-Identifier: CC0-1.0
 //
-// SPDX-FileContributor: Antonio Niño Díaz, 2023
+// SPDX-FileContributor: Antonio Niño Díaz, 2023-2024
+
+// There are two exception handlers in libnds:
+//
+// - The debug exception handler is setup by default in debug builds of libnds,
+//   and it prints a lot of information to the screen (like the address where
+//   the crash happened and the values of the CPU registers at that point).
+//
+// - The release exception handler only prints a minimal error message.
+//
+// If you want to use the debug handler in release builds, just call
+// defaultExceptionHandler() in your code.
+//
+// In order to use a debug build of libnds, link to "-lnds9d" in your Makefile
+// instead of "-lnds9".
+//
+// This example links to the debug version of libnds to show the debug exception
+// handler, but you can switch to the release version to see that handler too.
 
 #include <stdio.h>
 
@@ -8,6 +25,9 @@
 
 int main(int argc, char **argv)
 {
+    // As explained above, this isn't required in debug builds, only in release
+    // builds. In debug builds this is called before reaching main(). In release
+    // builds, this replaces the minimal release exception handler.
     defaultExceptionHandler();
 
     consoleDemoInit();
